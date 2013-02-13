347bd92012b640fd2b87df2f8655e0d1eefe43de
sfagmenos <g.koloventzos@gmail.com>
2013-02-13 21:20:16 +0000

    changing prototype class
    git-svn-id: file:///Users/sfagmenos/svn@84 4d3cfc12-b59a-4ddc-a5fe-3228cfc53caa

--- glowscript/eucleidis.js
+++ glowscript/eucleidis.js
@@ -3,7 +3,7 @@ GlowScript 1.0
 scene.width = scene.height = 500
 scene.range = 2.2
 
-scene.caption.text("From VPython to Glowscript")
+scene.title.text("From VPython to Glowscript")
 
 
 sphere( {pos:vec(0,0,0), size:1*vec(1,1,1),visible:false}) // display a box for context
@@ -11,23 +11,26 @@ var points =0;
 var allpoints = [];
 
 function Point_2(locate){
-    this.location=locate;
+    this.locatio=locate;
     this.point = sphere ({pos:locate, size:0.1*vec(1,1,1)} );
     this.x =function (){return this.locate.pos.x;};
     this.y =function (){return this.locate.pos.y;};
     this.z =function (){return this.locate.pos.z;};
-    this.ll = function(){return this.location};
-    }
-Point_2.prototype.add = function(p2){
-    //var xa = this.x+p2.x;
-//    var ya = this.y+p2.y;
-  //  var za = this.z+p2.z;
-    return Point_2({pos:(this.ll+p2.ll)});
+    this.ll = function(){return this.locatio};
 }
 
-Point_2.prototype.equal = function(p2){
-    return this.x == p2.x && this.y == p2.y && this.z == p2.z;
-}
+Point_2.prototype = {
+    constructor : Point_2,
+    add : function(p2){
+        var bla = this.locatio + p2.locatio;
+        var p = Point_2(bla);
+        return p;
+    }
+};
+
+//Point_2.prototype.equal = function(p2){
+//    return this.x == p2.x && this.y == p2.y && this.z == p2.z;
+//}
 
 
 function Segment_2(start,end){
@@ -44,11 +47,7 @@ function Segment_2(start,end){
         var ym = start.y+end.y;
         var zm = start.z+end.z;
         return Point_2({pos:vec(xm,ym,zm)});
-    };
-    
-}
-function Line_2(){
-    
+    };   
 }
 
     scene.waitfor("click",wait)
@@ -57,5 +56,5 @@ function Line_2(){
     scene.waitfor("click",wait)
     var loc1 = scene.mouse.pos
     allpoints[1] = new Point_2(loc1);
-    var s = allpoints[0].add(allpoints[1])
-    var seg = new Segment_2(allpoints[0],allpoints[1]);
+    allpoints[0].add(allpoints[1])
+    //var seg = new Segment_2(allpoints[0],allpoints[1]);
